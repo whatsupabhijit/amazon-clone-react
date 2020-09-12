feea29e8f31c27ac3919be4687e4e49d4c856ff7
Abhijit Dutta <whatsupabhijit@gmail.com>
2020-09-12 17:58:46 +0000

    Fixed login issue. Implemented Auth feature in App.js, Header.js and Login.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/js/Header";
 import Home from "./components/js/Home";
 import Login from "./components/js/Login";
 import Checkout from "./components/js/Checkout";
-// import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+    auth.onAuthStateChanged((authUser) => {
+      // console.log("THE USER IS >>> ", authUser);
+      if (authUser) {
+        // the user just logged in / the user was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <div className="app">
