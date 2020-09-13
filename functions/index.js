b9f4fe5e8f4c815b5e329a37d026685528cf2e6e
Abhijit Dutta <whatsupabhijit@gmail.com>
2020-09-13 08:48:59 +0000

    Created backend server with express, cors, firebase functions. firebase init was done to choose functions instead of hosting first. Then made sure to enter functions directory and installed the backend libs like express using npm

--- functions/index.js
+++ functions/index.js
@@ -7,15 +7,31 @@ const stripe = require("stripe")(
 
 // API
 
-// App config
+// - App config
 const app = express();
 
-// Middlewares
+// - Middlewares
 app.use(cors({ origin: true }));
-app.use(express.json);
+app.use(express.json());
 
-// API - routes
-app.get("/", (req, res) => res.status(200).send("hello world"));
+// - API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "usd",
+  });
+
+  // OK - Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
 // Listen command
 exports.api = functions.https.onRequest(app);
