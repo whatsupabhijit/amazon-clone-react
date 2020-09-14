e6011addb9e0456dd072a59391bd8494f80eda4d
Abhijit Dutta <whatsupabhijit@gmail.com>
2020-09-14 07:37:42 +0000

    tried to change node in functinos from 10 to 8 and deployed Google firebase cloud functions. But still its not working in production hosted version. Its working with emulators 'firebase emulators:start' at backend

--- src/axios.js
+++ src/axios.js
@@ -2,9 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL:
-    // "https://us-central1-challenge-4b2b2.cloudfunctions.net/api",
-    "http://localhost:5001/clone-react-133ca/us-central1/api",
+  baseURL: "https://us-central1-clone-react-133ca.cloudfunctions.net/api",
+  // "http://localhost:5001/clone-react-133ca/us-central1/api",
 });
 
 export default instance;
